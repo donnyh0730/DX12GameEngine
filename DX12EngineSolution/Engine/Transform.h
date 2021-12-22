4a7164e99120f07c37fde9a532f5f9b858495eed
donnyh0730 <donnyh0730@gmail.com>
2021-12-22 15:34:37 +0900

    Frustum Culling -by Sort Object mechanism

--- DX12EngineSolution/Engine/Transform.h
+++ DX12EngineSolution/Engine/Transform.h
@@ -16,6 +16,9 @@ public:
 	const Vec3& GetLocalRotation() { return _localRotation; }
 	const Vec3& GetLocalScale() { return _localScale; }
 
+	// TEMP
+	float GetBoundingSphereRadius() { return max(max(_localScale.x, _localScale.y), _localScale.z); }
+
 	const Matrix& GetLocalToWorldMatrix() { return _matWorld; }
 	Vec3 GetWorldPosition() { return _matWorld.Translation(); }
 
